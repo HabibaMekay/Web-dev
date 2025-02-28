cb9834233aa2041cf6b2bbd2f31b615c2ef7484b
HabibaMekay <habibamekay@gmail.com>
2025-02-28 12:32:17 +0200

    fixed connection string

--- Assignment1/books.js
+++ Assignment1/books.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/booksdb', {
+mongoose.connect('mongodb://localhost:27017/booksbd', { 
   serverSelectionTimeoutMS: 30000, 
 });
 
