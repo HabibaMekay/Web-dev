a743d467a32c5511f19b1de1c154e114b4c185f9
HabibaMekay <habibamekay@gmail.com>
2025-03-14 21:19:54 +0200

    fixed el dumb error

--- Assignment3/routes/authors/authorR.js
+++ Assignment3/routes/authors/authorR.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { Author } = require('./author'); 
-
+const Author = require('./author'); 
 
 
 router.get('/', async (req, res) => {
