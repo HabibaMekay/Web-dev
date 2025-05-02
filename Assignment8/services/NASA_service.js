0026e01bb640faf45db5372e377738f5fb78f0a4
HabibaMekay <habibamekay@gmail.com>
2025-05-02 22:52:05 +0300

    done inshallah

--- Assignment8/services/NASA_service.js
+++ Assignment8/services/NASA_service.js
@@ -1,7 +1,6 @@
 const axios = require('axios');
 
 async function fetchNASA() {
-  console.log(process.env.NASA_API_KEY)
   try {
     const response = await axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY");
     return response.data;
